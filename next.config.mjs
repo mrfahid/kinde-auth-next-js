aea8152ec26cf3f76f03bc8c8d6d6f430d760d41
Shah Fahid <135055359+mrfahid@users.noreply.github.com>
2024-05-28 11:39:18 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ['media.licdn.com'],
   },
-  env: {
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
-  },
 };
 
 export default nextConfig;
